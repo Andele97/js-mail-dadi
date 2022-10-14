ae3247503238def8758ffa21e7cc6a799932e161
Andele97 <andreapinna40@live.it>
2022-10-14 17:39:16 +0200

    [errore] messaggio

--- e-mail/js/script.js
+++ e-mail/js/script.js
@@ -3,14 +3,29 @@
 //stampa un messaggio appropriato sull’esito del controllo.
 
 const listEmail = ['andrea@live.it', 'cristian@live.it', 'lina@live.it'];
-let found = false;
+let found;
 
 const logButton = document.querySelector("button");
-const emailSelect = document.querySelector('[name:"email"]');
+const emailSelect = document.querySelector('[name="email"]');
 
 logButton.addEventListener("click", function() {
 
+  const output = document.querySelector('.output');
+  output.innerHTML = "";
+
   for (let i = 0; i < listEmail.length; i++){
-    
+    if (emailSelect === listEmail[i]){
+      found = true;
+    }else{
+      found = false;
+    }
+  }
+
+  if (found = listEmail) {
+    output.append('email trovata')
+    output.classList.add('green')
+  }else{
+    output.append('email non trovata')
+    output.classList.add('red')
   }
 })
