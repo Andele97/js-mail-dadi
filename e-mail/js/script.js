6c3c955425ac64e19afc7385c814a59ef45f7de7
Andele97 <andreapinna40@live.it>
2022-10-14 16:44:43 +0200

    [add] structure and variable

---
+++ e-mail/js/script.js
@@ -0,0 +1,16 @@
+//Chiedi all’utente la sua email,
+//controlla che sia nella lista di chi può accedere,
+//stampa un messaggio appropriato sull’esito del controllo.
+
+const listEmail = ['andrea@live.it', 'cristian@live.it', 'lina@live.it'];
+let found = false;
+
+const logButton = document.querySelector("button");
+const emailSelect = document.querySelector('[name:"email"]');
+
+logButton.addEventListener("click", function() {
+
+  for (let i = 0; i < listEmail.length; i++){
+    
+  }
+})
